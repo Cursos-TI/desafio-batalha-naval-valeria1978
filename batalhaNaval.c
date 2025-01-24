7c815ac72d1a0590986f92e115aac2d281fb24b6
valeria1978 <117307139+valeria1978@users.noreply.github.com>
2025-01-24 06:07:09 -0300

    Update batalhaNaval.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,342 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h> //habilita limpeza do buffer de teclado no linux
+#define L 15
+#define C 15
+#define TRUE 1
+#define FALSE 0
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+void zerarMatriz(char m[L][C]);
+void exibeMatriz(char m[L][C]);
+void montaTabuleiro (char m[L][C]);
+void recebeValores(char m[L][C], int tam, char nav);
+int inserirPeca(char m[L][C], int posLinha, int posCol, int dir, int tam, char nav);
+int confereJogo(char m[L][C]);
+int atirar(char m[L][C]);
+void exibirJogo(char m[L][C]);
+
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+	char m1[L][C], m2[L][C];
+	int i = 0;
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+	//zerando as matrizes com água
+	zerarMatriz(m1);
+	zerarMatriz(m2);
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+	//MONTAGEM DOS TABULEIROS
+	exibeMatriz(m1);
+	printf("\n\n=================== PLAYER 1: ====================\n\n");
+	montaTabuleiro(m1);
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+	exibeMatriz(m2);
+	printf("\n\n=================== PLAYER 2: ====================\n\n");
+	montaTabuleiro(m2);
 
-    return 0;
+	printf("\n\n========== TUDO PRONTO! VAMOS AO JOGO! ===========\n\n");
+
+	do {
+		i++;
+		printf("%s\n", ((i % 2) == 0)? "\n--------- PLAYER 2 --------\n": "\n--------- PLAYER 1 --------\n");
+
+		do {
+			exibirJogo(((i % 2) == 0)? m1: m2);
+			printf("\nPREPARAR CANHÕES!!!\n");
+
+		} while(atirar(((i % 2) == 0)? m1: m2) == TRUE);
+
+	} while(confereJogo(m1) == FALSE && confereJogo(m2) == FALSE);
+
+	if(confereJogo(m2) == TRUE) {
+		printf("\n============================================================\n");
+		printf("PARABÉNS!!! Player 1 é o vencedor e MASSACROU o player 2!!!!");
+		printf("\n============================================================\n\n");
+	}
+	else {	
+		printf("\n============================================================\n");
+		printf("PARABÉNS!!! Player 2 é o vencedor e MASSACROU o player 1!!!!");
+		printf("\n============================================================\n\n");
+	}
+
+	return 0;
+} 
+
+
+//ZERA A MATRIZ COM AGUA
+void zerarMatriz(char m[L][C]) {
+	int i, j;
+
+	//zerando tudo com 'a'
+	for(i=0; i<L; i++) {
+		for(j=0; j<C; j++) {
+			m[i][j] = 'a';
+		}
+	}
+}
+
+//EXIBE A MATRIZ
+void exibeMatriz(char m[L][C]){
+	int i, j;
+	char v[L];
+
+	printf("\n\t     ");
+
+	//preenche vetor com as letras do indice
+	for(i=0; i<L; i++) {
+		v[i] = (char) 'A' + i;
+
+		//ao mesmo tempo que preenche as letras printa os numeros do indice
+		if(i >= 8) {
+			printf("%d ", i+1);
+		}
+		else {
+			printf("%d  ", i+1); //só altera o espaço entre os valores com 2 casas decimais
+		}
+	}
+
+	printf("\n");
+
+	for(i=0; i<L; i++) {
+		printf("\t(%c)  ", v[i]);
+		for(j=0; j<C; j++) {
+			printf("%c  ", m[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+//CONSTROI TABULEIRO
+void montaTabuleiro(char m[L][C]) {
+	int i;
+
+	printf("Posicionar Porta Aviões (5 casas): \n");
+	recebeValores(m, 5, 'P');
+	exibeMatriz(m);
+
+	printf("Posicionar Cruzador (4 casas): \n");
+	recebeValores(m, 4, 'C');
+	exibeMatriz(m);
+
+	for(i=0; i<2; i++) {
+		printf("Posicionar %dº Contra-Torpedeiro (3 casas): \n", i + 1);
+		recebeValores(m, 3, 'T');
+		exibeMatriz(m);
+	}
+
+	for(i=0; i<2; i++) {
+		printf("Posicionar %dª Rebocador (2 casas): \n", i + 1);
+		recebeValores(m, 2, 'R');
+		exibeMatriz(m);
+	}
+
+	for(i=0; i<4; i++) {
+		printf("Posicionar %dª Submarino: (1 casa): \n", i + 1);
+		recebeValores(m, 1, 'S');
+		exibeMatriz(m);
+	}
+}
+
+//RECEBE OS VALORES
+void recebeValores(char m[L][C], int tam, char nav) {	
+	char linha, dir;
+	int coluna;
+
+	do {
+		__fpurge(stdin); //limpando buffer de teclado no linux
+		fflush(stdin);	 //limpando buffer de teclado no windows
+
+		printf("Linha: ");
+		scanf("%c", &linha);
+
+		printf("Coluna: ");
+		scanf("%d", &coluna);
+		
+		__fpurge(stdin); //limpando buffer de teclado no linux
+		fflush(stdin);   //limpando buffer de teclado no windows
+
+		if(tam > 1) {
+			printf("Direção (h ou v): ");
+			scanf("%c", &dir);
+		}
+
+	} while(inserirPeca(m, linha, coluna -1, dir, tam, nav) == FALSE);
+
+}
+
+//INSERI AS PEÇAS
+int inserirPeca(char m[L][C], int posLinha, int posCol, int dir, int tam, char nav){
+	int i;
+	int iPosLinha;
+
+	//passando caracter para inteiro
+	iPosLinha = (int) posLinha - 97; //97 = 'a' em ascii
+
+	switch(dir) {
+		case 'h':
+			if((posCol + tam) <= C) {  		//confere se a peça cabe nos limites da matriz
+				for(i = 0; i < tam; i++) {  //confere se já existe um navio na posição inserida
+					if(m[iPosLinha][posCol + i] != 'a') {
+						printf("\n\nJá existe um navio posicionado aí! Tente outra posição.\n\n");
+						return FALSE;
+					}
+				}
+
+				for(i = 0; i < tam; i++) {  //faz a inserção
+					m[iPosLinha][posCol + i] = nav;
+				}
+
+				return TRUE;
+			}
+			else {
+				printf("\n\nA peça ficou fora dos limites do tabuleiro. Por favor tente outra posição.\n\n");
+				return FALSE;
+			}
+
+		case 'v':
+			if((iPosLinha + tam) <= L) {
+				for(i = 0; i < tam; i++) {
+					if(m[iPosLinha + i][posCol] != 'a') {
+						printf("\n\nJá existe um navio posicionado aí! Tente outra posição.\n\n");
+						return FALSE;
+					}
+				}
+
+				for(i = 0; i < tam; i++) {
+					m[iPosLinha + i][posCol] = nav;			
+				}
+
+				return TRUE;
+			}
+			else {
+				printf("\n\nA peça ficou fora dos limites do tabuleiro. Por favor tente outra posição.\n\n");
+				return FALSE;
+			}
+
+		default:
+			printf("\n\nDireção Inválida! Tente novamente.\n\n");
+			return FALSE;
+	}
+
+}
+
+//CONFERE SE O JOGO TERMINOU
+int confereJogo(char m[L][C]) {
+	int i, j;
+
+	for(i=0; i<L; i++) {
+		for(j=0; j<C; j++) {
+			if(m[i][j] != 'a' && m[i][j] != 'X' && m[i][j] != 'A') {
+				return FALSE;
+			}
+		}
+	}
+
+	return TRUE;
+}
+
+//EXECUTA A JOGADA
+int atirar(char m[L][C]) {
+	int iLinha, coluna;
+	char linha;
+
+	__fpurge(stdin); 	 //limpando buffer de teclado no linux
+		fflush(stdin);	 //limpando buffer de teclado no windows
+
+	printf("\nLinha: ");
+	scanf("%c", &linha);
+	iLinha = (int) linha - 97;
+
+	__fpurge(stdin); 	 //limpando buffer de teclado no linux
+		fflush(stdin);	 //limpando buffer de teclado no windows
+
+	printf("Coluna: ");
+	scanf("%d", &coluna);
+	coluna--;
+
+	__fpurge(stdin); 	 //limpando buffer de teclado no linux
+		fflush(stdin);	 //limpando buffer de teclado no windows
+
+	printf("\nATIRAR!!!!! ");
+
+	switch(m[iLinha][coluna]) {
+		case 'a' : 
+			printf("OOOOOH, NÃO! ÁGUA...\n");
+			m[iLinha][coluna] = 'A';
+			return FALSE;
+
+		case 'X' :
+			printf("Você já atirou aí, marujo... Sai do canhão!\n");
+			return FALSE;
+
+		case 'P' :
+			printf("BOA, MARUJO! ACERTOU O PORTA AVIÕES!! BELO TIRO!\n");
+			m[iLinha][coluna] = 'X';
+			return TRUE;
+
+		case 'C' :
+			printf("BOA, MARUJO! ACERTOU UM CRUZADOR!! BELO TIRO!\n");
+			m[iLinha][coluna] = 'X';
+			return TRUE;
+
+		case 'T' :
+			printf("BOA, MARUJO! ACERTOU UM CONTRA-TORPEDEIRO!! BELO TIRO!\n");
+			m[iLinha][coluna] = 'X';
+			return TRUE;
+
+		case 'R' :
+			printf("BOA, MARUJO! ACERTOU UM REBOCADOR!! BELO TIRO!\n");
+			m[iLinha][coluna] = 'X';
+			return TRUE;
+
+		case 'S' :
+			printf("BOA, MARUJO! ACERTOU UM SUBMARINO!! BELO TIRO!\n");
+			m[iLinha][coluna] = 'X';
+			return TRUE;
+	}
+}
+
+//EXIBE O JOGO SÓ COM AS CASAS JOGADAS
+void exibirJogo(char m[L][C]) {
+	int i, j;
+	char v[L];
+
+	printf("\n\t     ");
+
+	//preenche vetor com as letras do indice
+	for(i=0; i<L; i++) {
+		v[i] = (char) 'A' + i;
+
+		//ao mesmo tempo que preenche as letras printa os numeros do indice
+		if(i >= 8) {
+			printf("%d ", i+1);
+		}
+		else {
+			printf("%d  ", i+1); //só altera o espaço entre os valores com 2 casas decimais
+		}
+	}
+
+	printf("\n");
+
+	for(i=0; i<L; i++) {
+		printf("\t(%c)  ", v[i]);
+		for(j=0; j<C; j++) {
+			switch(m[i][j]) {
+				case 'X' :
+					printf("X  ");
+					break;
+
+				case 'A' :
+					printf("A  ");
+					break;
+
+				default :
+					printf("?  ");
+					break;
+			}
+		}
+		printf("\n");
+	}
+	printf("\n");
 }
